ca318b8d34aff041bf8ce66d599cbde473daa225
Sofiyyah <sofiyyah.abidoye@decagon.dev>
2023-05-10 12:49:42 +0100

    reset password page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import {Container, Button, Title, Count} from './style'
 import MyComponent from './Context/Context';
+import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
+import ResetPassword from './page/ResetPassword/ResetPassword';
 
 function App() {
 
   return (
-    <div>
-      <MyComponent></MyComponent>
-      <Container>
-      <Title>My App</Title>
-      <Count>Count: 0</Count>
-      <Button onClick={() => console.log('Button clicked!')}>
-        Click me
-      </Button>
-    </Container>
-      
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route element = {<ResetPassword/>} path="/reset-password"/>
+      </Routes>
+    </Router>
+    </>
   )
 }
 
