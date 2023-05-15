059d4c5d1cb7607f543f9b1aafb8449a31f69669
Angiephoenix <angiephoenix93@gmail.com>
2023-05-15 22:02:31 +0100

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
 import React from 'react'
-import {Container, Button, Title, Count} from './style'
-import MyComponent from './Context/Context';
 import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 import ResetPassword from './page/ResetPassword/ResetPassword';
 import AboutUs from "./page/AboutUs Page/AboutUs";
-import { LandingPage } from './Component/LandiingPage Component/LandingPage';
+
 
 export function App() {
   return (
@@ -13,7 +11,6 @@ export function App() {
     <>
     <Router>
       <Routes> 
-      <Route path="/" element={<LandingPage />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route element = {<ResetPassword/>} path="/reset-password"/>
       </Routes>
