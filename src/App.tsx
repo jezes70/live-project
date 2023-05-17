9064ca2ea35f58bddea99b898b2ed4a2a0d957c4
lotodee <olamidotun.loto@decagon.dev>
2023-05-17 13:50:37 +0100

    hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import {Container, Button, Title, Count} from './style'
-import MyComponent from './Context/Context';
 import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 import ResetPassword from './page/ResetPassword/Reset';
 import Forgot from './page/ForgotPassword/Forgot';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { LandingPage } from './Component/LandiingPage Component/LandingPage';
+import { LandingPage } from './page/LandingPage/LandingPage';
 
 
 function App() {
--- src/Component/LandiingPage Component/LandingPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Hero from "./Hero"
-export const LandingPage = () => {
-  return (
-    <div>
-      <Hero/>
-    </div>
-  )
-}
