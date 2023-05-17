f744463c6ca9f2935cd62dc1afd87c6d94d47888
OlawaleP <99323873+OlawaleP@users.noreply.github.com>
2023-05-17 12:23:16 +0100

    registration_frontpage_design

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import {Container, Button, Title, Count} from './style'
 import MyComponent from './Context/Context';
 import {Route, Routes, BrowserRouter as Router} from 'react-router-dom';
 import ResetPassword from './page/ResetPassword/ResetPassword';
+import { Registration } from "./page/UserRegistration Page/userRegistration";
 
 function App() {
-
   return (
     <>
     <Router>
       <Routes>
-        <Route element = {<ResetPassword/>} path="/reset-password"/>
+        <Route element={<ResetPassword/>} path="/reset-password"/>
+        <Route element={<Registration />} path="/register" />
       </Routes>
     </Router>
     </>
   )
 }
 
-export default App
+export default App;
