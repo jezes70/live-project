8652403bb5ba344b935296fec6b1159f9a6f37a1
Okpara Ifeanyi Ekene <123643727+jezes70@users.noreply.github.com>
2023-05-17 03:00:48 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Button, Title, Count } from "./style";
+import MyComponent from "./Context/Context";
+import { LoginPage } from "./page/Login Page/LoginPage";
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/Login Page/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Login } from "../../Component/LoginPage Component/LonginPage";
+
+export const LoginPage = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
