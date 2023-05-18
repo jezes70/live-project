b8e7f684b4a5ca469ed04e4966a4be9759593e9f
lotodee <olamidotun.loto@decagon.dev>
2023-05-18 16:56:31 +0100

    hero-section

--- src/Component/Hero/Hero.tsx
+++ src/Component/Hero/Hero.tsx
@@ -1,33 +1,35 @@
-import React from "react"
-import "./hero.css"
+import React from "react";
+import "./hero.css";
+import {Link } from "react-router-dom"
 
-export default function Landing() {
+export default function Hero() {
   return (
     <div>
-
-     <main>
-
-    <section id="a">
-
-<h1>Mental Health Matters</h1>
-
-<p id="numberOne">Share, connect, and provide support in a safe space for mental health. A community of empatics where you get to unburden your mind.</p>
-
-<p id="numberTwo">Mental health conditions affect millions of people worldwide. Many people struggle to find the support they need to cope with their conditions, 
-   stigma often prevents them from seeking help.
-
-</p>
-
-<button id="first">Sign In</button>
-
-<button id="second">Register Here</button>
-
-    </section>
-    <section id="b">
-<img src="./images/Frame 8409 (3).png" alt="logo" height="640" width="617"/>
-    </section>
-  </main>
+      <main>
+        <section id="a">
+          <h1>Mental Health Matters</h1>
+          <p id="numberOne">
+            Share, connect, and provide support in a safe space for mental
+            health. A community of empathics where you get to unburden your
+            mind.
+          </p>
+          <p id="numberTwo">
+            Mental health conditions affect millions of people worldwide. Many
+            people struggle to find the support they need to cope with their
+            conditions, stigma often prevents them from seeking help.
+          </p>
+          <button id="first">Sign In</button>
+          <button id="second">Register Here</button>
+        </section>
+        <section id="b">
+          <img
+            src="./images/Frame 8409 (3).png"
+            alt="logo"
+            height="640"
+            width="617"
+          />
+        </section>
+      </main>
     </div>
-
-  )
+  );
 }
--- src/page/LandingPage/LandingPage.tsx
+++ src/page/LandingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Hero from "../../Component/Hero/Hero"
+import Navbar from "../../Component/Navbar/Navbar"
 export const LandingPage = () => {
   return (
     <div>
+      <Navbar/>
       <Hero/>
     </div>
   )
