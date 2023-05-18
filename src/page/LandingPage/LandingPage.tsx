0c1f8aea4b69d9b83b4fe423eb3d2f9819c5232c
Edititus <tediomo.et@gmail.com>
2023-05-18 17:24:38 +0100

    landingpagefour

--- src/page/LandingPage/LandingPage.tsx
+++ src/page/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../../Component/Hero/Hero";
 import About from "../../Component/AboutUs/About";
+import LandingPageFour from "../../Component/EnjoyPrivacyAndInclusivity/LandingPageFour";
 export const LandingPage = () => {
   return (
     <div>
       <Hero />
       <About /> 
+      <LandingPageFour />
     </div>
   );
 };
